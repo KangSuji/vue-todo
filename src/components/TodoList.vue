d5fd58d4f6867e8e04cde3521d86db77172ec3ba
KangSuji <sukang0608@naver.com>
2022-01-27 11:29:46 +0900

    트랜지션 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li 
         class="shadow"
         v-for="(todoItem, index) in propsdata" 
@@ -18,7 +18,7 @@
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>  
+    </transition-group>  
   </div>
 </template>
 
@@ -72,5 +72,12 @@ li {
   color: #de4343;
   cursor: pointer;
 }
-
+/* 리스트 아이템 트랜지션 효과 */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
